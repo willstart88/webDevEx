25a6f37f23cc41ed7ae5b6799a73118fdf429306
willstart88 <waiyet@gmail.com>
2020-04-28 11:16:10 +0800

    Initial commit

---
+++ 12_JS Stalker Exercise/script.js
@@ -0,0 +1,5 @@
+const firstName = prompt("What is your first name?");
+const lastName = prompt("What is your last name?");
+const age = prompt("What is your age?");
+alert("Your full name is " + firstName+" "+ lastName)
+alert("You are " +age + " years old")
